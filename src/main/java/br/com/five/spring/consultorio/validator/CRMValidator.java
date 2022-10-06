9a3215ffd382cfa83436a06bf41d4a713ce5a2d7
teste <teste123124@gmail.com>
2022-10-06 12:13:14 -0300

    correcao bug regex crm

--- src/main/java/br/com/five/spring/consultorio/validator/CRMValidator.java
+++ src/main/java/br/com/five/spring/consultorio/validator/CRMValidator.java
@@ -13,7 +13,7 @@ public class CRMValidator implements ConstraintValidator<ValidarCRM, String> {
 	
 	private Pattern pattern;
 	private Matcher matcher;
-	private static final String CRM_PATTERN = "^\\d{6}\\/\\\\\\d{2}[a-zA-Z]$";
+	private static final String CRM_PATTERN = "^[0-9]{6}\\/[A-Z]{2}$";
 	
 	@Override
 	public void initialize(ValidarCRM constraintAnnotation) {
